5510da7daa0e5f15b21c3106dd403bb62dcd93b0
Some docker company <somedockercompany@gmail.com>
2019-03-11 20:00:33 +0100

    Add doc

--- docker/build.sh
+++ docker/build.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
+#Copy the jar to this folder
 cp ../target/*-jar-with-dependencies.jar app.jar
+
+#Create tarball archive
 tar -cf app.tar.gz app.jar
 
+#Build the docker image
 docker build -t somedockercompany/simple-rest-app:latest .
 
+#Login to Docker Hub
 docker login
 
+#Push the image to Docker Hub
 docker push somedockercompany/simple-rest-app:latest
 
+#Delete the tarball file
 rm -rf app.jar app.tar.gz
