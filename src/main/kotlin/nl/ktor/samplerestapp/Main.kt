485241f0a7c85b271f32623a1ba8a7448d111e53
Some docker company <somedockercompany@gmail.com>
2019-03-11 19:10:33 +0100

    Create docker compose file

--- src/main/kotlin/nl/ktor/samplerestapp/Main.kt
+++ src/main/kotlin/nl/ktor/samplerestapp/Main.kt
@@ -7,12 +7,15 @@ import io.ktor.routing.get
 import io.ktor.routing.routing
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
+import java.util.*
+
+val randomNumber = UUID.randomUUID();
 
 fun main() {
     val server = embeddedServer(Netty, port = 8080) {
         routing {
             get("/") {
-                call.respondText("Hello World! a", ContentType.Text.Plain)
+                call.respondText("Hello World! UUID: $randomNumber", ContentType.Text.Plain)
             }
             get("/demo") {
                 call.respondText("HELLO WORLD!")
