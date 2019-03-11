c5eb092d41d4870bce00244d38be4a7ff4ac4c28
Some docker company <somedockercompany@gmail.com>
2019-03-11 14:38:10 +0100

    add docker login

--- docker/build.sh
+++ docker/build.sh
@@ -5,6 +5,8 @@ tar -cf app.tar.gz app.jar
 
 docker build -t somedockercompany/simple-rest-app:latest .
 
+docker login
+
 docker push somedockercompany/simple-rest-app:latest
 
 rm -rf app.jar app.tar.gz
