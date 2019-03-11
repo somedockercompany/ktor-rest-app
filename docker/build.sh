d591dc97eeaec4cb598d279dec12b08b969ad3aa
Some docker company <somedockercompany@gmail.com>
2019-03-11 13:10:16 +0100

    Change docker repo name

--- docker/build.sh
+++ docker/build.sh
@@ -3,6 +3,6 @@
 cp ../target/*-jar-with-dependencies.jar app.jar
 tar -cf app.tar.gz app.jar
 
-docker build -t com2ghz/simple-rest-app .
+docker build -t somedockercompany/simple-rest-app .
 
 rm -rf app.jar app.tar.gz
