e5f5f6238437f2fefbda564efa48de74ed281cc9
Some docker company <somedockercompany@gmail.com>
2019-03-11 13:21:50 +0100

    push the image

--- docker/build.sh
+++ docker/build.sh
@@ -3,6 +3,8 @@
 cp ../target/*-jar-with-dependencies.jar app.jar
 tar -cf app.tar.gz app.jar
 
-docker build -t somedockercompany/simple-rest-app .
+docker build -t somedockercompany/simple-rest-app:latest .
+
+docker push somedockercompany/simple-rest-app:latest
 
 rm -rf app.jar app.tar.gz
